ea5409e7ddaa581c77a844ea27b7a0af2d26f1f5
nNGUYEN TINH <HOANGTINHQK@GMAIL.COM>
2021-12-10 23:16:43 +0700

    thêm tính năng pagination vào New products với query truyền lên là page

--- src/app/controllers/newProController.js
+++ src/app/controllers/newProController.js
@@ -1,15 +1,33 @@
 const Fabric = require('../../app/models/Fabric');
 const { multipleMongooseToObject } = require('../../util/mongoose');
 const { mongooseToObject } = require('../../util/mongoose');
+const PAGE_SIZE = 6;
 
 class newProController {
     // [GET] newPro
     index(req, res, next) {
-        Fabric.find()
-            .then(fabric => res.render('./newProduct/newPro.hbs', {
-                fabric: multipleMongooseToObject(fabric)
-            }))
-            .catch(next);
+        var page = req.query.page
+        if(page) {
+            page = parseInt(page)
+            var skip = (page - 1) * PAGE_SIZE;
+            
+            Fabric.find({})
+                .skip(skip)
+                .limit(PAGE_SIZE)
+                .then(fabric => res.render('./newProduct/newPro.hbs', {
+                    fabric: multipleMongooseToObject(fabric)
+                }))
+                .catch(next)
+
+        }else{
+            // get all
+
+            Fabric.find()
+                .then(fabric => res.render('./newProduct/newPro.hbs', {
+                    fabric: multipleMongooseToObject(fabric)
+                }))
+                .catch(next);
+        }
     }
 
     show(req, res, next) {
