fe455f6af38f5f6464975c011f81052f45e6483f
nNGUYEN TINH <HOANGTINHQK@GMAIL.COM>
2021-12-10 22:33:15 +0700

    clear comment ở Model Fabric

--- src/app/models/Fabric.js
+++ src/app/models/Fabric.js
@@ -1,29 +1,26 @@
-const mongoose = require('mongoose')// import thư viện mongoose
-const Schema = mongoose.Schema;// tạo 1 biến với phương thức Schema về để 
-const slug = require('mongoose-slug-generator'); // call đến plugin tải về mongoose để có thể 
-                                // thêm được slug sau local/Fabrics/slug
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
 
 const mongooseDelete = require('mongoose-delete');
 
 
 const Fabric = new Schema({
-    name: {type: 'string', required: true},// truyền về giá trị Name, required là phải có 
-    description: {type: 'string'},// mô tả với giá trị là chuỗi
-    image: {type: 'string'},// tương tự mô tả
-    slug: { type: String, slug: 'name', unique: true},// truyền chuổi về, giá trị slug lấy giống name
-                                            // unique là mỗi slug là duy nhất khác thì truyền thêm id vào 
+    name: {type: 'string', required: true},
+    description: {type: 'string'},
+    image: {type: 'string'},
+    slug: { type: String, slug: 'name', unique: true},
     price: {type: String}
 },{
-    timestamps:true,// cập nhật thời gian update và creat 
+    timestamps:true,
     collection: 'fabrics'
 });
 
 // add plugin
-mongoose.plugin(slug)// thêm phương thức plugin với giá trị là slug
+mongoose.plugin(slug)
 Fabric.plugin(mongooseDelete, { 
     deletedAt: true,
     overrideMethods: 'all', 
-})// thêm plugin xóa mềm 
+})
 
-module.exports = mongoose.model('Fabric', Fabric); //xuất giá trị với dạng model
-                                            // với biến Fabric có tên là Fabric
+module.exports = mongoose.model('Fabric', Fabric);
