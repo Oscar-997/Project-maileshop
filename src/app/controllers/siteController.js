e439b2279585b865a0e3541c27c14ac2dbc3c54c
nNGUYEN TINH <HOANGTINHQK@GMAIL.COM>
2022-01-29 17:03:37 +0700

    thêm logo từ database về thanh header

--- src/app/controllers/siteController.js
+++ src/app/controllers/siteController.js
@@ -1,15 +1,17 @@
 const Slider = require('../../app/models/Slider')
 const Info = require('../models/Info')
-const { multipleMongooseToObject } = require('../../util/mongoose');
+const InfoShop = require('../models/InfoShop')
+const { multipleMongooseToObject, mongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] home
     index(req, res, next) {
 
-        Promise.all([Slider.find({}), Info.find({})])
-            .then(([sliders, infos]) =>  res.render('home', {
+        Promise.all([Slider.find({}), Info.find({}), InfoShop.findOne({})])
+            .then(([sliders, infos, infoShops]) =>  res.render('home', {
                 sliders: multipleMongooseToObject(sliders),
                 infos: multipleMongooseToObject(infos),
+                infoShops: mongooseToObject(infoShops)
             }))
             .catch(next);
     }
